aafc12e3c1f0d260efb0223dfe27840594ff295b
MoreAmazingNick <github@nicolas-schneider.at>
2024-12-05 16:07:35 +0100

    permission fix

--- templates/0BasicFiles/configuration.php
+++ templates/0BasicFiles/configuration.php
@@ -3,7 +3,6 @@
 /** @var \Icinga\Application\Modules\Module $this */
 
 $section = $this->menuSection(N_('__Modulename__'), [
-    'permission' => '__modulename__',
     'url' => '__modulename__/classic',
     'icon' => 'beaker',
     'priority' => 910
